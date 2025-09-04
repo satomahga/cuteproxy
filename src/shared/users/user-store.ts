8b9159910a5faac32cef38c28076dafa2abf9fd9
Astera <astera.vt@proton.me>
2025-09-04 15:25:06 +0300

    ensure prompt quota is updated daily

--- src/shared/users/user-store.ts
+++ src/shared/users/user-store.ts
@@ -841,10 +841,7 @@ function maybeResetPromptCounts(user: User) {
   if (!resetAt || resetAt <= now) {
     meta.promptCounts = {};
     meta.promptCountsResetAt = getNextUtcMidnight(now);
-<<<<<<< HEAD
-=======
     meta.promptCountsResetNote = "UpdatesAt";
->>>>>>> c06ab01 (ensure prompt quota is updated daily)
     usersToFlush.add(user.token);
   }
 }
